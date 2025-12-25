139a4d5418e52bfe6e9f3e9d334c67b1819fe5fc
Le Biller Guillaume <guillaume.lebiller@gmail.com>
2025-12-25 18:37:20 -0500

    feat: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/satisfactory-planner/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        transformations: resolve(__dirname, 'transformations.html'),
+      },
+    },
+  },
 });
